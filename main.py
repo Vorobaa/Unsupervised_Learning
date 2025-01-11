37031602d5e9c3ba30652a0de3c08523ebb9b440
Vorobaa <vorobaaa72@gmail.com>
2025-01-11 12:29:08 +0200

    Method KMeans with 2 centroids

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import centroid
 from sklearn.cluster import KMeans
 
 np.random.seed(42)
@@ -9,4 +10,18 @@ plt.scatter(X[:,0], X[:,1], s=50, color='gray')
 plt.title('Output data')
 plt.xlabel('X1')
 plt.ylabel('X2')
+plt.show()
+
+kmeans = KMeans(n_clusters=2, random_state=42)
+kmeans.fit(X)
+
+labels = kmeans.labels_
+centroids = kmeans.cluster_centers_
+
+plt.scatter(X[:,0], X[:,1], c=labels, cmap='viridis', s=50)
+plt.scatter(centroids[:,0], centroids[:,1], c='red', marker='x', s=200, label='Центроїд')
+plt.title('Результат кластеризації')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.legend()
 plt.show()
